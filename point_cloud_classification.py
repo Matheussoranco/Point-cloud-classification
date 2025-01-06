d4d66a3525e87443d5f0bae5dc7de14bdfff4965
Matheus Pullig Soranço de Carvalho <matheussoranco@gmail.com>
2025-01-06 07:12:34 -0300

    Parse dataset

--- point_cloud_classification.py
+++ point_cloud_classification.py
@@ -29,3 +29,33 @@ ax = fig.add_subplot(111, projection="3d")
 ax.scatter(points[:, 0], points[:, 1], points[:, 2])
 ax.set_axis_off()
 plt.show()
+
+def parse_dataset(num_points=2048):
+    train_points = []
+    train_labels = []
+    test_points = []
+    test_labels = []
+    class_map = {}
+    folders = glob.glob(os.path.join(DATA_DIR, "[!README]*"))
+    
+    for i, folder in enumerate(folders):
+        print("Processando classe: {}".format(os.path.basename(folder)))
+        class_map[i] = folder.split("/")[-1]
+        train_files = glob.glob(os.path.join(folder, "train/*"))
+        test_files = glob.glob(os.path.join(folder, "test/*"))
+        
+        for f in train_files:
+            train_points.append(trimesh.load(f).sample(num_points))
+            train_labels.append(i)
+            
+        for f in test_files:
+            test_points.append(trimesh.load(f).sample(num_points))
+            test_labels.append(i)
+            
+    return(
+        np.array(train_points),
+        np.array(test_points),
+        np.array(train_labels),
+        np.array(test_labels),
+        class_map,
+    )
