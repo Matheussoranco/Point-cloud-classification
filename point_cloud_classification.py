96ecdc6513bb881fc4640d5bfaf0639f3aef2f61
Matheus Pullig Soranço de Carvalho <matheussoranco@gmail.com>
2025-01-07 09:09:00 -0300

    Tranformation input fix

--- point_cloud_classification.py
+++ point_cloud_classification.py
@@ -107,3 +107,22 @@ def dense_bn(x, filters):
     x = layers.Dense(filters)(x)
     x = layers.BatchNormalization(momentum=0.0)(x)
     return layers.Activation("relu")(x)
+
+def tnet(inputs, num_features):
+    bias = keras.initializers.Constant(np.eye(num_features).flatten())
+    reg = OrthogonalRegularizer(num_features)
+
+    x = conv_bn(inputs, 32)
+    x = conv_bn(x, 64)
+    x = conv_bn(x, 512)
+    x = layers.GlobalMaxPooling1D()(x)
+    x = dense_bn(x, 256)
+    x = dense_bn(x, 128)
+    x = layers.Dense(
+        num_features * num_features,
+        kernel_initializer="zeros",
+        bias_initializer=bias,
+        activity_regularizer=reg,
+    )(x)
+    feat_T = layers.Reshape((num_features, num_features))(x)
+    return layers.Dot(axes=(2, 1))([inputs, feat_T])
