5a110c8b21605fd949e0c91bad475e1e4eb9b96e
Matheus Pullig Soranço de Carvalho <matheussoranco@gmail.com>
2025-01-07 09:11:54 -0300

    Visualize Training

--- point_cloud_classification.py
+++ point_cloud_classification.py
@@ -154,3 +154,26 @@ model.compile(
 )
 
 model.fit(train_dataset, epochs=20, validation_data=validation_dataset)
+
+data = test_dataset.take(1)
+
+points, labels = list(data)[0]
+points = points[:8, ...]
+labels = labels[:8, ...]
+
+preds = model.predict(points)
+preds = ops.argmax(preds, -1)
+
+points = points.numpy()
+
+fig = plt.figure(figsize=(15, 10))
+for i in range(8):
+    ax = fig.add_subplot(2, 4, i + 1, projection="3d")
+    ax.scatter(points[i, :, 0], points[i, :, 1], points[i, :, 2])
+    ax.set_title(
+        "pred: {:}, label: {:}".format(
+            CLASS_MAP[preds[i].numpy()], CLASS_MAP[labels.numpy()[i]]
+        )
+    )
+    ax.set_axis_off()
+plt.show()
