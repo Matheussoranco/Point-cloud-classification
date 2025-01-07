64347164b374dab3c6f0f321961ede948d9c1793
Matheus Pullig Soranço de Carvalho <matheussoranco@gmail.com>
2025-01-07 09:10:59 -0300

    Train model

--- point_cloud_classification.py
+++ point_cloud_classification.py
@@ -146,3 +146,11 @@ outputs = layers.Dense(NUM_CLASSES, activation="softmax")(x)
 
 model = keras.Model(inputs=inputs, outputs=outputs, name="pointnet")
 model.summary()
+
+model.compile(
+    loss="sparse_categorical_crossentropy",
+    optimizer=keras.optimizers.Adam(learning_rate=0.001),
+    metrics=["sparse_categorical_accuracy"],
+)
+
+model.fit(train_dataset, epochs=20, validation_data=validation_dataset)
