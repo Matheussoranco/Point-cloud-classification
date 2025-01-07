73a3eb5054a2b6bce36f0064200a9c933f86ae78
Matheus Pullig Soranço de Carvalho <matheussoranco@gmail.com>
2025-01-07 09:05:36 -0300

    Building model

--- point_cloud_classification.py
+++ point_cloud_classification.py
@@ -84,3 +84,14 @@ test_dataset = test_dataset.shuffle(len(test_points)).batch(BATCH_SIZE)
 
 train_dataset = dataset.take(train_dataset_size).batch(BATCH_SIZE)
 validation_dataset = dataset.skip(train_dataset_size).batch(BATCH_SIZE)
+
+def conv_bn(x, filters):
+    x = layers.Conv1D(filters, kernel_size=1, padding="valid")(x)
+    x = layers.BatchNormalization(momentum=0.0)(x)
+    return layers.Activation("relu")(x)
+
+
+def dense_bn(x, filters):
+    x = layers.Dense(filters)(x)
+    x = layers.BatchNormalization(momentum=0.0)(x)
+    return layers.Activation("relu")(x)
